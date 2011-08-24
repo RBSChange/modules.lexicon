1a20f5ed734d989fe4174d155660c4b809506f00
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-24 11:52:36 +0200

    [NEW] Indexation refactoring

--- persistentdocument/word.class.php
+++ persistentdocument/word.class.php
@@ -1,19 +1,5 @@
 <?php
-class lexicon_persistentdocument_word extends lexicon_persistentdocument_wordbase implements indexer_IndexableDocument
+class lexicon_persistentdocument_word extends lexicon_persistentdocument_wordbase
 {
-	/**
-	 * Get the indexable document
-	 * @return indexer_IndexedDocument
-	 */
-	public function getIndexedDocument()
-	{
-		$indexedDoc = new indexer_IndexedDocument();
-		$indexedDoc->setId($this->getId());
-		$indexedDoc->setDocumentModel('modules_lexicon/word');
-		$indexedDoc->setLabel($this->getLabel());
-		$indexedDoc->setLang(RequestContext::getInstance()->getLang());
-		$indexedDoc->setText(f_util_StringUtils::htmlToText($this->getDefinition()));
-		Framework::debug(__METHOD__ . var_export($indexedDoc, true));
-		return $indexedDoc;
-	}
+
 }
