3088aa8b320db2c4663f60c90a6f73382171d99a
Gaël PORT <gael.port@rbs.fr>
2012-03-16 15:53:17 +0100

    [FIX] Minor fixes an refactoring.

--- lib/blocks/BlockWordContextuallistAction.class.php
+++ lib/blocks/BlockWordContextuallistAction.class.php
@@ -14,15 +14,14 @@ class lexicon_BlockWordContextuallistAction extends website_BlockAction
 	}
 
 	/**
-	 * @see f_mvc_Action::execute()
-	 *
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
 	 * @return String
 	 */
-	function execute($request, $response)
+	public function execute($request, $response)
 	{
 		// Get the parent topic
+		$ls = LocaleService::getInstance();
 		$ancestor = $this->getPage()->getAncestors();
 		$topicId = f_util_ArrayUtils::lastElement($ancestor);
 		$wordService = lexicon_WordService::getInstance();
@@ -36,8 +35,10 @@ class lexicon_BlockWordContextuallistAction extends website_BlockAction
 		$firstletter = $request->getParameter('fl');
 		$navigation = array();
 
-		$allTermsNav =  array('label' => f_Locale::translate('&modules.lexicon.frontoffice.n;'),
-         'title' => f_Locale::translate('&modules.lexicon.frontoffice.all-terms-title;'));
+		$allTermsNav = array(
+			'label' => $ls->transFO('m.lexicon.frontoffice.n', array('html')),
+        	'title' => $ls->transFO('m.lexicon.frontoffice.all-terms-title', array('attr'))
+		);
 		if ($firstletter != "")
 		{
 			$allTermsNav['href'] = LinkHelper::getCurrentUrl(array('lexiconParam' => array('fl' => '', paginator_Paginator::PAGEINDEX_PARAMETER_NAME => 1)));
@@ -46,8 +47,10 @@ class lexicon_BlockWordContextuallistAction extends website_BlockAction
 
 		foreach ($letters as $letter)
 		{
-			$link = array('label' => f_Locale::translate('&modules.lexicon.frontoffice.N' . $letter . ';'),
-          'title' => f_Locale::translate('&modules.lexicon.frontoffice.a-letter-terms;', array("letter" => $letter)));
+			$link = array(
+				'label' => $ls->transFO('m.lexicon.frontoffice.n' . $letter, array('ucf', 'html')),
+				'title' => $ls->transFO('m.lexicon.frontoffice.a-letter-terms', array('attr'), array("letter" => $letter))
+			);
 			if ($firstletter != $letter)
 			{
 				$link['href'] = LinkHelper::getCurrentUrl(array('lexiconParam' => array('fl' => $letter, paginator_Paginator::PAGEINDEX_PARAMETER_NAME => 1)));
@@ -56,7 +59,7 @@ class lexicon_BlockWordContextuallistAction extends website_BlockAction
 		}
 
 		$request->setAttribute('navigation', $navigation);
-		$request->setAttribute('current', f_Locale::translate('&modules.lexicon.frontoffice.N' . $firstletter . ';'));
+		$request->setAttribute('current', $ls->transFO('m.lexicon.frontoffice.n' . $firstletter, array('ucf', 'html')));
 
 		$items = $wordService->getWordsByTopicsByLetter($topicId, $firstletter);
 
@@ -65,7 +68,7 @@ class lexicon_BlockWordContextuallistAction extends website_BlockAction
 
 		// Set the paginator
 		$paginator = new paginator_Paginator('lexicon', $request->getParameter(paginator_Paginator::PAGEINDEX_PARAMETER_NAME, 1), $items, $nbItemPerPage);
-		$paginator->setListName(f_Locale::translate('&modules.lexicon.frontoffice.terms-listName;'));
+		$paginator->setListName($ls->transFO('m.lexicon.frontoffice.terms-listname'));
 		$request->setAttribute('paginator', $paginator);
 
 		return website_BlockView::SUCCESS;
